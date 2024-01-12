dca74406e23bd75858f68958046537496088a9d8
birdie871014 <105782539+birdie871014@users.noreply.github.com>
2024-01-12 17:38:22 +0000

    cotinue session feature added

--- ScreenGPT.py
+++ ScreenGPT.py
@@ -3,28 +3,41 @@ import streamlit as st
 import json
 import requests
 
-
+#functions
 def post_to_jsonbin():
     url = 'https://api.jsonbin.io/v3/b'
     headers = {
     'Content-Type': 'application/json',
     'X-Master-Key': st.secrets["jsonBinKey"]
     }
-    st.session_state.data = {
-        "age" : st.session_state.age,
-        "sex" : st.session_state.sex,
-        "weight" : st.session_state.weight,
-        "height" : st.session_state.height,
-        "messages" : st.session_state.messages
-    }
+    st.session_state.data["messages"] = st.session_state.messages
+    
     st.session_state["bin"] = requests.post(url, json=st.session_state.data, headers=headers)
+    st.session_state["sessionID"] = st.session_state.bin.json()["metadata"]["id"]
 
-def put_to_jsonbin():
+def read_from_jsonbin(id):
+    url = f'https://api.jsonbin.io/v3/b/{id}/latest'
+    headers = {
+    'X-Master-Key': st.secrets["jsonBinKey"]
+    }
+    response = requests.get(url, json=None, headers=headers)
+    if response.status_code == 200:
+        st.session_state['read_ok'] = True
+        st.session_state['data'] = json.loads(response.text)['record']
+        st.session_state['messages'] = st.session_state.data['messages']
+        return True
+    else: 
+        st.session_state['readerror'] = True
+        st.session_state['sessionID'] = ""
+        return False
+
+
+def put_to_jsonbin(id):
     if "update_counter" not in st.session_state:
         st.session_state["update_counter"] = 1
     else:
         st.session_state.update_counter += 1
-    url = f'https://api.jsonbin.io/v3/b/{st.session_state.bin.json()["metadata"]["id"]}'
+    url = f'https://api.jsonbin.io/v3/b/{id}'
     headers = {
     'Content-Type': 'application/json',
     'X-Master-Key': st.secrets["jsonBinKey"]
@@ -41,42 +54,85 @@ def set_lang(lang):
 def collect_ok():
     st.session_state["collect_status"] = True
 
+#select language
+with st.sidebar:
+    st.image('./banner.png', use_column_width=True)
+    if "language" not in st.session_state:
+        st.text('Please select language!')
+        col1, col2, blankcol = st.columns([0.25, 0.25, 0.5]) 
+        col1.button(label='EN', on_click=set_lang, args=['english'])
+        col2.button(label='HU', on_click=set_lang, args=['hungarian'])
 
+#variable definitions
+if "data" not in st.session_state:
+    st.session_state['data'] = {
+        "age" : 0,
+        "sex" : "Male",
+        "weight" : 0,
+        "height" : 0,
+    }
+if "read_ok" not in st.session_state:
+    st.session_state['read_ok'] = False
+if "collect_status" not in st.session_state:
+    st.session_state['collect_status'] = False
+if "readerror" not in st.session_state:
+    st.session_state['readerror'] = False
+if "show_form" not in st.session_state:
+    st.session_state['show_form'] = True
+
+#heading
 col_logo, col_head = st.columns([0.2, 0.8])
 with col_logo:
     st.image("./logo.png", use_column_width=True)
 with col_head:
-    st.title("Welcome to ScreenGPT 👨🏽‍⚕️")
-
-    col1, col2, col3, col4 = st.columns([0.4, 0.15, 0.15, 0.3])
-    col1.text('Please select language!')
-    col2.button(label='EN', on_click=set_lang, args=['english'])
-    col3.button(label='HU', on_click=set_lang, args=['hungarian'])
+    st.title("Welcome to ScreenGPT 👨🏽‍⚕️ beta")
+    
 
 
 if "language" in st.session_state:
     texts = {}
     with open("./lang.json") as io:
         texts = json.load(io)[st.session_state.language]
-    with st.form(key='collect_data', ):
-        st.text(texts["form_header"])
-        st.session_state["age"] = st.number_input(label=texts['age'])
-        st.session_state["sex"] = st.selectbox(texts['sex'], [texts['male'], texts['female']])
-        st.session_state["weight"] = st.number_input(label=texts["weight"])
-        st.session_state["height"] = st.number_input(label=texts["height"])
-        st.form_submit_button(label="OK", on_click=collect_ok)
-
     
-if "collect_status" in st.session_state:
-    client = OpenAI(api_key=st.secrets["OpenaiKey"])
+
+    with st.sidebar:
+        
+        if st.session_state.readerror == True:
+                st.write(texts['readeror'])
+        
+        if st.session_state.show_form:    
+            with st.form(key='collect_data'):
+                st.write(texts["form_header"])
+                st.session_state.data["age"] = st.number_input(label=texts['age'])
+                st.session_state.data["sex"] = st.selectbox(texts['sex'], [texts['male'], texts['female']])
+                st.session_state.data["weight"] = st.number_input(label=texts["weight"])
+                st.session_state.data["height"] = st.number_input(label=texts["height"])
+                st.form_submit_button(label="OK", on_click=collect_ok)
+        
+        if st.session_state.collect_status == False:
+            st.write(texts['askForCode'])
+            st.session_state['sessionID'] = st.text_input('ID')
+            if len(st.session_state.sessionID) > 0 and st.session_state.read_ok == False:
+                st.session_state['collect_status'] = read_from_jsonbin(st.session_state.sessionID)
+                st.rerun()
+        
+        
+    
+
     if st.session_state.collect_status:
+        client = OpenAI(api_key=st.secrets["OpenaiKey"])
+        with st.sidebar:
+            st.write(texts['write_ID'])
+            st.text(st.session_state.sessionID)
+            st.write(texts['ID_description'])
         if "messages" not in st.session_state:            
-            st.session_state["messages"] = [{"role": "system", "content": f"In this conversation you are the assistant developed for assist lifestyle change. At first introduce yourself. The user who asks is {st.session_state.age} years old {st.session_state.height} cm tall and has {st.session_state.weight} kg body weight. The language of this conversation is {st.session_state.language}. The gender of the user in the language of the conversation is '{st.session_state.sex}'. Please give personalized answers, and allways note the gender. In your answer evaluate users BMI index! If it is out of the normal range, give an advice how to reach the ideal body weight. You are a healthcare tool. Answer only the questions about lifestyle change!"}]
+            st.session_state["messages"] = [{"role": "system", "content": f"In this conversation you are the assistant developed for assist lifestyle change. At first introduce yourself. The user who asks is {st.session_state.data['age']} years old {st.session_state.data['height']} cm tall and has {st.session_state.data['weight']} kg body weight. The language of this conversation is {st.session_state.language}. The gender of the user in the language of the conversation is '{st.session_state.data['sex']}'. Please give personalized answers, and allways note the gender. In your answer evaluate users BMI index! If it is out of the normal range, give an advice how to reach the ideal body weight. You are a healthcare tool. Answer only the questions about lifestyle change!"}]
             response = client.chat.completions.create(model="gpt-4", temperature=0.2, messages=st.session_state.messages)
             msg = response.choices[0].message.content
-            #msg = st.session_state.sex + " " + str(st.session_state.age) + " " + str(st.session_state.height) + " " + str(st.session_state.weight)
             st.session_state.messages.append({"role": "assistant", "content": msg})
             post_to_jsonbin()
+            st.session_state['show_form'] = False
+            st.rerun()
         
         for msg in st.session_state.messages:
             if msg["role"] != "system":
@@ -89,7 +145,7 @@ if "collect_status" in st.session_state:
             msg = response.choices[0].message.content
             st.session_state.messages.append({"role": "assistant", "content": msg})
             st.chat_message("assistant").write(msg)
-            put_to_jsonbin()
+            put_to_jsonbin(st.session_state.sessionID)
 
 
 
