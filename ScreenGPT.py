e4f2f5b560fc66a6b0a3c738385dd0d5c0b19a92
birdie871014 <105782539+birdie871014@users.noreply.github.com>
2024-01-23 11:20:59 +0000

    language selection and form moved back to the main screen,

--- ScreenGPT.py
+++ ScreenGPT.py
@@ -3,7 +3,6 @@ import streamlit as st
 import json
 import requests
 
-st.set_page_config(initial_sidebar_state="expanded")
 
 #functions
 
@@ -58,15 +57,6 @@ def set_lang(lang):
 def collect_ok():
     st.session_state["collect_status"] = True
 
-#select language
-with st.sidebar:
-    st.image('./images/banner.png', use_column_width=True)
-    if "language" not in st.session_state:
-        st.text('Please select language!')
-        col1, col2, blankcol = st.columns([0.25, 0.25, 0.5]) 
-        col1.button(label='EN', on_click=set_lang, args=['english'])
-        col2.button(label='HU', on_click=set_lang, args=['hungarian'])
-
 #variable definitions
 if "data" not in st.session_state:
     st.session_state['data'] = {
@@ -89,41 +79,48 @@ if "sessionID" not in st.session_state:
 
 #heading
 col_logo, col_head = st.columns([0.2, 0.8])
-with col_logo:
-    st.image("./images/logo.png", use_column_width=True)
-with col_head:
-    st.title("Welcome to ScreenGPT 👨🏽‍⚕️ beta")
-    
+col_logo.image("./images/logo.png", use_column_width=True)
+col_head.title(":blue[Welcome to ScreenGPT 👨🏽‍⚕️ beta]")
+with st.sidebar:
+    st.image("./images/banner.png", use_column_width=True)
+#select language
+
+if "language" not in st.session_state:
+    st.title(':violet[Please select language!]')
+    col1, col2, col3, col4 = st.columns([0.4, 0.1, 0.1, 0.4]) 
+    col2.button(label='EN', on_click=set_lang, args=['english'])
+    col3.button(label='HU', on_click=set_lang, args=['hungarian'])
+
+
 if "language" in st.session_state:
 #load texts
     texts = {}
     with open("./lang.json") as io:
         texts = json.load(io)[st.session_state.language]
     
-    with st.sidebar:
-    #display read error
-        if st.session_state.readerror == True:
-                st.write(texts['readeror'])
-    #form
-        if st.session_state.show_form:    
-            with st.form(key='collect_data'):
-                st.write(texts["form_header"])
-                st.session_state.data["age"] = st.number_input(label=texts['age'])
-                st.session_state.data["sex"] = st.selectbox(texts['sex'], [texts['male'], texts['female']])
-                st.session_state.data["weight"] = st.number_input(label=texts["weight"])
-                st.session_state.data["height"] = st.number_input(label=texts["height"])
-                st.markdown(f"<p style='text-align: justify; font-size: 12px'>{texts['disclaimer']}</p>", unsafe_allow_html=True)
-                st.form_submit_button(label="OK", on_click=collect_ok)
-                if st.session_state.collect_status == True and len(st.session_state.sessionID) == 0:
-                    st.write(texts['wait'])
-    #load session
-        if st.session_state.collect_status == False:
-            st.write(texts['askForCode'])
-            st.session_state['sessionID'] = st.text_input('ID')
-            if len(st.session_state.sessionID) > 0 and st.session_state.read_ok == False:
-                if read_from_jsonbin(st.session_state.sessionID):
-                    collect_ok()
-                st.rerun()
+#display read error
+    if st.session_state.readerror == True:
+            st.write(texts['readeror'])
+#form
+    if st.session_state.show_form:    
+        with st.form(key='collect_data'):
+            st.write(texts["form_header"])
+            st.session_state.data["age"] = st.number_input(label=texts['age'])
+            st.session_state.data["sex"] = st.selectbox(texts['sex'], [texts['male'], texts['female']])
+            st.session_state.data["weight"] = st.number_input(label=texts["weight"])
+            st.session_state.data["height"] = st.number_input(label=texts["height"])
+            st.markdown(f"<p style='text-align: justify; font-size: 12px'>{texts['disclaimer']}</p>", unsafe_allow_html=True)
+            st.form_submit_button(label="OK", on_click=collect_ok)
+            if st.session_state.collect_status == True and len(st.session_state.sessionID) == 0:
+                st.write(texts['wait'])
+#load session
+    if st.session_state.collect_status == False:
+        st.write(texts['askForCode'])
+        st.session_state['sessionID'] = st.text_input('ID')
+        if len(st.session_state.sessionID) > 0 and st.session_state.read_ok == False:
+            if read_from_jsonbin(st.session_state.sessionID):
+                collect_ok()
+            st.rerun()
     #chat started
     if st.session_state.collect_status:
         #create openAI class object
@@ -149,8 +146,6 @@ if "language" in st.session_state:
         for msg in st.session_state.messages:
             if msg["role"] != "system":
                 st.chat_message(msg["role"]).write(msg["content"])
-        #write out session ID under the chat
-        st.markdown(f"<p style='text-align: right; font-size: 12px'>Session ID : {st.session_state.sessionID}</p>", unsafe_allow_html=True)
         #input and answer user prompt
         if prompt := st.chat_input():
             st.session_state.messages.append({"role": "user", "content": prompt})
@@ -160,10 +155,18 @@ if "language" in st.session_state:
             st.session_state.messages.append({"role": "assistant", "content": msg})
             st.chat_message("assistant").write(msg)
             put_to_jsonbin(st.session_state.sessionID)
+        #write out session ID under the chat
+        st.markdown(f"""<div style='border: 2px solid blue; border-radius: 10px; color: white; background-color: blue; position: fixed; bottom: 110px; right: 20px'>
+                            <p style='color: white; text-align: right; font-size: 12px'>&#160;&#160;&#160; Session ID : {st.session_state.sessionID} &#160;&#160;&#160;</p>
+                        </div>""", unsafe_allow_html=True)
+
 #linkedin logo        
 with st.sidebar:
     st.markdown("""
-                <a href='https://www.linkedin.com/company/screengpt/about/'>
-                    <img src='https://logos-world.net/wp-content/uploads/2020/05/Linkedin-Logo.png', alt='follow us on Linkedin', border=0, width=100px>
-                </a>
+                <div style="position: fixed; bottom: 0px">
+                    <p>Follow Us!</p>
+                    <a href='https://www.linkedin.com/company/screengpt/about/'>
+                        <img src='https://logos-world.net/wp-content/uploads/2020/05/Linkedin-Logo.png', alt='follow us on Linkedin', border=0, width=100px>
+                    </a>
+                </div>
                 """, unsafe_allow_html=True)
