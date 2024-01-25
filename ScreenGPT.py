60f79b42066be2b0b8f7bc0b04d9caa9ef4b0c12
birdie871014 <105782539+birdie871014@users.noreply.github.com>
2024-01-25 08:54:00 +0000

    zero value verification inseted

--- ScreenGPT.py
+++ ScreenGPT.py
@@ -45,9 +45,9 @@ def put_to_jsonbin(id):
     url = f'https://api.jsonbin.io/v3/b/{id}'
     headers = {
     'Content-Type': 'application/json',
-    'X-Master-Key': st.secrets["jsonBinKey"]
+    'X-Master-Key': st.secrets['jsonBinKey']
     }
-    st.session_state.data["messages"] = st.session_state.messages
+    st.session_state.data['messages'] = st.session_state.messages
     st.session_state[f"jsonbin_put_response{st.session_state.update_counter}"] = requests.put(url, json=st.session_state.data, headers=headers)
 
 def set_lang(lang):
@@ -56,7 +56,10 @@ def set_lang(lang):
     st.session_state.language = lang
 
 def collect_ok():
-    st.session_state["collect_status"] = True
+    if any(v == 0 for v in st.session_state['data'].values()):
+        st.warning(texts['zero_value_warning'])
+    else:
+        st.session_state['collect_status'] = True
 
 #variable definitions
 if "data" not in st.session_state:
@@ -106,10 +109,10 @@ if "language" in st.session_state:
     if st.session_state.show_form:    
         with st.form(key='collect_data'):
             st.write(texts["form_header"])
-            st.session_state.data["age"] = st.number_input(label=texts['age'])
+            st.session_state.data["age"] = st.number_input(label=texts['age'], min_value=0, step=1)
             st.session_state.data["sex"] = st.selectbox(texts['sex'], [texts['male'], texts['female']])
-            st.session_state.data["weight"] = st.number_input(label=texts["weight"])
-            st.session_state.data["height"] = st.number_input(label=texts["height"])
+            st.session_state.data["weight"] = st.number_input(label=texts["weight"], min_value=0, step=1)
+            st.session_state.data["height"] = st.number_input(label=texts["height"], min_value=0, step=1)
             st.markdown(f"<p style='text-align: justify; font-size: 12px'>{texts['disclaimer']}</p>", unsafe_allow_html=True)
             st.form_submit_button(label="OK", on_click=collect_ok)
             if st.session_state.collect_status == True and len(st.session_state.sessionID) == 0:
@@ -157,15 +160,15 @@ if "language" in st.session_state:
             st.chat_message("assistant").write(msg)
             put_to_jsonbin(st.session_state.sessionID)
         #write out session ID under the chat
-        st.markdown(f"""<div style='border: 2px solid blue; border-radius: 10px; color: white; background-color: blue; position: fixed; bottom: 110px; right: 33%'>
-                            <p style='color: white; text-align: right; font-size: 12px'>&#160;&#160;&#160; Session ID : {st.session_state.sessionID} &#160;&#160;&#160;</p>
+        st.markdown(f"""<div style='border: 2px solid blue; border-radius: 10px; color: white; background-color: blue; position: fixed; bottom: 128px; right: 30%'>
+                            <p style='margin-bottom: 0; color: white; text-align: right; font-size: 12px'>&#160;&#160;&#160; Session ID : {st.session_state.sessionID} &#160;&#160;&#160;</p>
                         </div>""", unsafe_allow_html=True)
 
 #linkedin logo        
 with st.sidebar:
     st.markdown("""
-                <div style="position: fixed; bottom: 0px">
-                    <p>Follow Us!</p>
+                <div style="text-align: center; height: 80px; width:120px;  position: fixed; bottom: 0px; background-color: white; border: 2px solid white; border-radius: 18px">
+                    <p style="margin-bottom: 0; color: black">Follow Us!</p>
                     <a href='https://www.linkedin.com/company/screengpt/about/'>
                         <img src='https://logos-world.net/wp-content/uploads/2020/05/Linkedin-Logo.png', alt='follow us on Linkedin', border=0, width=100px>
                     </a>
